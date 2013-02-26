cda239e7f39b704ebb4f072aa0cd6d37e00b7dc5
Will Furnass <will@thearete.co.uk>
2013-02-26 20:43:07 +0000

    Error checking for invalid diameters

--- pyhyd.py
+++ pyhyd.py
@@ -6,5 +6,7 @@ def x_sec_area(D):
     D -- internal diameter (m)
      
     """
+    if np.any(D <=0):
+        raise ValueError("Non-positive internal pipe diam.")
     return np.pi * np.power((0.5 * D), 2)
 
